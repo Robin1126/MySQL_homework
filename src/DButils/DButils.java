a1a83065d3fa53c62fc017ab8db029691615050f
Binbin Luo <luobinb7@gmail.com>
2023-04-29 23:23:01 +0200

    mysql homework

--- src/DButils/DButils.java
+++ src/DButils/DButils.java
@@ -1,2 +1,51 @@
-package DButils;public class DButils {
+package DButils;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class DButils {
+
+    // 加载驱动
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DButils() {
+    }
+
+    // 获取连接对象
+    public static Connection getConnection() throws SQLException {
+        ResourceBundle rb = ResourceBundle.getBundle("Info");
+        return DriverManager.getConnection(rb.getString("url"),rb.getString("userName"),rb.getString("password"));
+    }
+
+    // 释放资源
+    public static void close(ResultSet rs, Statement ps, Connection conn) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/homework01/Homework01.java
+++ src/homework01/Homework01.java
@@ -1,2 +1,47 @@
-package homework01;public class Homework01 {
+package homework01;
+
+import DButils.DButils;
+
+import java.sql.*;
+
+/*
+    1、取得每个部门最高薪水的人员名称
+        第一步：先获得每个部门的最高薪水 注意 select中只能是分组函数和分组的字段，其他的字段没有意义
+        第二步：将上面的临时表t和emp表连接，连接条件是deptno相等，sal和maxsal相等
+
+ */
+public class Homework01 {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement ps = null;
+        ResultSet rs = null;
+
+        // 1. 注册驱动，获取连接 使用DButils工具
+        try {
+            conn = DButils.getConnection();
+            // 3 获取数据库操作对象
+            String sql =
+                    "select " +
+                        "e.ename, e.deptno, t.maxsal " +
+                    "from " +
+                        "emp e " +
+                    "join" +
+                        " (select deptno, max(sal) maxsal from emp  group by deptno) t " +
+                    "on " +
+                        "e.deptno = t.deptno and e.sal = t.maxsal";
+
+            ps = conn.createStatement();
+            rs = ps.executeQuery(sql);
+//            System.out.println("ename\t maxsal\t deptno");
+            while (rs.next()) {
+                System.out.println(rs.getString("ename") + ", " + rs.getString("maxsal") + ", " + rs.getString("deptno"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DButils.close(rs, ps, conn);
+        }
+    }
 }
