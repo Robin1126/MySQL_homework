3887f7eec3d4d05951ef98e360bfea7374bb1c1c
luobinbin <luobinb7@gmail.com>
2023-05-01 12:52:59 +0200

    vpntest

--- src/homework02/homework2.java
+++ src/homework02/homework2.java
@@ -1,6 +1,7 @@
 package homework02;
 
 import DButils.DButils;
+import com.mysql.cj.protocol.Resultset;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,6 +10,7 @@ public class homework2 {
     public static void main(String[] args) {
         try {
             Connection conn = DButils.getConnection();
+            Resultset rs = null;
         } catch (SQLException e) {
             e.printStackTrace();
         }
